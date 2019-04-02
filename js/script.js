47315926d2a7a8a4c975fe1483b3cdef3491ec06
Stijn <Stijn.heylen@brainworx.be>
2019-04-02 21:25:11 +0200

    First version table

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ jQuery(function($){
 		$editor = $('#editor'),
 		$editorTitle = $('#editor-title'),
 		ft = FooTable.init('#locations-overview', {
-			columns: $.get("../../content/columns.json"),
-			rows: $.get("../../content/rows.json"),
+			columns: $.get("config/columns.json"),
+			rows: $.get("config/rows.json"),
 			editing: {
 				addRow: function(){
 					$modal.removeData('row');
@@ -15,14 +15,25 @@ jQuery(function($){
 				editRow: function(row){
 					var values = row.val();
 					$editor.find('#id').val(values.id);
-					$editor.find('#firstName').val(values.firstName);
-					$editor.find('#lastName').val(values.lastName);
-					$editor.find('#jobTitle').val(values.jobTitle);
-					$editor.find('#status').val(values.status);
-					$editor.find('#startedOn').val(values.started.format('YYYY-MM-DD'));
-					$editor.find('#dob').val(values.dob.format('YYYY-MM-DD'));
+					$editor.find('#name').val(values.Name);
+					$editor.find('#apb').val(values.Apbnumber);
+					$editor.find('#address').val(values.Address);
+					$editor.find('#zipcode').val(values.Zipcode);
+					$editor.find('#city').val(values.City);
+					$editor.find('#phone').val(values.Phone);
+					$editor.find('#email').val(values.Email);
+					$editor.find('#website').val(values.Website);
+					$editor.find('#mon').val(values.Mon);
+					$editor.find('#tue').val(values.Tue);
+					$editor.find('#wed').val(values.Wed);
+					$editor.find('#thu').val(values.Thu);
+					$editor.find('#fri').val(values.Fri);
+					$editor.find('#sat').val(values.Sat);
+					$editor.find('#sun').val(values.Sun);
+					$editor.find('#state').val(values.State);
+					//$editor.find('#dob').val(values.dob.format('YYYY-MM-DD'));
 					$modal.data('row', row);
-					$editorTitle.text('Edit row #' + values.id);
+					$editorTitle.text('Edit row #' + values.Id);
 					$modal.modal('show');
 				},
 				deleteRow: function(row){
@@ -40,12 +51,22 @@ jQuery(function($){
 		var row = $modal.data('row'),
 			values = {
 				id: $editor.find('#id').val(),
-				firstName: $editor.find('#firstName').val(),
-				lastName: $editor.find('#lastName').val(),
-				jobTitle: $editor.find('#jobTitle').val(),
-				started: moment($editor.find('#startedOn').val(), 'YYYY-MM-DD'),
-				dob: moment($editor.find('#dob').val(), 'YYYY-MM-DD'),
-				status: $editor.find('#status option:selected').val()
+				Name: $editor.find('#name').val(),
+				Apbnumber: $editor.find('#apb').val(),
+				Address: $editor.find('#address').val(),
+				Zipcode: $editor.find('#zipcode').val(),
+				City: $editor.find('#city').val(),
+				Phone: $editor.find('#phone').val(),
+				Email: $editor.find('#email').val(),
+				Website: $editor.find('#website').val(),
+				Mon: $editor.find('#mon').val(),
+				Tue: $editor.find('#tue').val(),
+				Wed: $editor.find('#wed').val(),
+				Thu: $editor.find('#thu').val(),
+				Fri: $editor.find('#fri').val(),
+				Sat: $editor.find('#sat').val(),
+				Sun: $editor.find('#sun').val(),
+				State: $editor.find('#state option:selected').val()
 			};
 
 		if (row instanceof FooTable.Row){
