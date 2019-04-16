cd66104d40af0ad910b0e2ec82388efaf84cce05
Brainworx <Brainworx@users.noreply.github.com>
2019-04-16 13:15:52 +0200

    fix update

--- exclusiveties/index.php
+++ exclusiveties/index.php
@@ -28,7 +28,8 @@ switch($_SERVER["REQUEST_METHOD"]) {
         parse_str(file_get_contents("php://input"), $_PUT);
 
         $result = $exlusiveties->update(array(
-          "location_id" => $_PUT["location_id"],
+			"id" => $_PUT["id"],
+			"location_id" => $_PUT["location_id"],
             "blocked_location_id" => $_PUT["blocked_location_id"],            
             "rule_active" => $_PUT["rule_active"]
         ));
